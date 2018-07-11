53433a728c9bc84040cbaea0b5f051628278fd50
wanghongrui <wanghongruigood@qq.com>
2018-07-11 15:36:21 +0800

    add mapboxgl

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BlockUI from 'vue-blockui'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
 Vue.use(BlockUI)
+Vue.use(VueLazyLoad)
 
 /* eslint-disable no-new */
 new Vue({
